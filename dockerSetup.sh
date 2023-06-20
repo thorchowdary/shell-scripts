9ecfe3cb0b32eabbb2194414460646f6c4eeaa8d
KETINENI TARUN <44497360+thorchowdary@users.noreply.github.com>
2023-06-20 08:15:50 +0530

    Update dockerSetup.sh

--- dockerSetup.sh
+++ dockerSetup.sh
@@ -1,5 +1,5 @@
 #1/bin/bash
 sudo yum install docker -y
 sudo systemctl start docker 
-sudo docker pull jenkins:2.60.3
-sudo docker run -d -p 1000:8080 jenkins:2.60.3
+docker pull jenkins/jenkins
+sudo docker run -d -p 1000:8080 jenkins/jenkins
